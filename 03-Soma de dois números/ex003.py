725f7aeecda1e3bd71d337f83f70b5acdb91a8c8
Tom Mereles <camposwellintom2@gmail.com>
2022-04-11 18:27:37 -0300

    Organizando

---
+++ 03-Soma de dois números/ex003.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor :'))
+result = n1 + n2
+print('A soma entre {}')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Estudos-Python
+
 Exercícios de python
