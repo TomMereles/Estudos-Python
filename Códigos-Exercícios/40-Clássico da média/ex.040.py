5fe3bfe642b480d57a98d50418642aa85359dc5e
Tom Mereles <camposwellintom2@gmail.com>
2022-04-27 16:21:37 -0300

    Add exercícios

---
+++ Códigos-Exercícios/40-Clássico da média/ex.040.py
@@ -0,0 +1,10 @@
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+média = (n1 + n2) / 2
+print(f'Tirando {n1} e {n2}, a média do aluno é {média:.1f}')
+if média >= 6:
+    print('O aluno está \033[1;32mAPROVADO\033[m.')
+elif média < 5:
+    print('O aluno está \033[1;31mREPROVADO\033[m. ')
+else:
+    print('O aluno está em \033[1;35mRECUPERAÇÃO\033[m.')
